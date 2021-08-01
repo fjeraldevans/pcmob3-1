c25430b4b347f0b6e54e65b7f5e2387733bf9eec
Jerald Evans <rooke.evans@gmail.com>
2021-08-01 19:26:51 +0800

    Added BlockRGB

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { React, useState } from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
+import { color } from "react-native-reanimated";
+
+function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 0, green: 255, blue: 0, id: "1" },
+    { red: 0, green: 0, blue: 255, id: "2" },
+  ]);
+
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
+  function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: `${colorArray.length}`,
+      },
+    ])
+  }
+
+ return (
+   <View style={styles.container}>
+     <TouchableOpacity
+     style={{ height: 40, justifyContent: 'center' }}
+     onPress={addColor}
+    >
+      <Text style={{ color: "red" }}>Add Colour</Text>
+    </TouchableOpacity>
+     <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
+   </View>
+ );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Home" component={HomeScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: "#fff",
+   alignItems: "center",
+   justifyContent: "center",
+ },
 });
+
+
